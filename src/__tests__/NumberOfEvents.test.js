c574fdfecd7edfc0e23b7c9b9c6fc6cbfe4b3aff
Borkkris <Borkkris@users.noreply.github.com>
2022-10-03 00:48:25 +0200

    style

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -19,15 +19,22 @@ describe('<NumberOfEvents /> component', () => {
         ).toEqual(32);
     });
 
+    test('display 32 if user input is not in range 1-32', () => {
+        NumberOfEventsWrapper.find('.number-of-events').simulate(
+            'change', {target: { value: 40 }}
+        );
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
+    });
+
     test('user change number of events', () => {
         NumberOfEventsWrapper.find('.number-of-events').simulate(
-            'change', {target: { value: 5 }}
+            'change', {target: { value: 4 }}
         );
-        expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(5);
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(4);
     });
 
     test('change numberOfEvents state when number input changes', () => {
-        NumberOfEventsWrapper.setState({ numberOfEvents: 10 });
+        NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
         NumberOfEventsWrapper.find('.number-of-events').simulate('change', { target: { value: 4 } });
         expect(NumberOfEventsWrapper.state('numberOfEvents')).not.toEqual(undefined);
         expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(4);
