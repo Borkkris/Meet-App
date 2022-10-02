132fb3454ebc101017737b1e8dbcac6d9a723ddb
Borkkris <Borkkris@users.noreply.github.com>
2022-10-03 00:55:12 +0200

    style

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ class App extends Component {
     const { locations, numberOfEvents } = this.state;
     return (
       <div className='App'>
+        
         <h1 className = 'appTitle'>Meet App</h1>
         <h4>Choose your nearest city</h4>
         {/* Displays the Components */}
