8645d0b7587594a3aa5fe091b4a56700d2f81cd9
Borkkris <Borkkris@users.noreply.github.com>
2022-10-15 18:15:41 +0200

    property to YAxis

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ class App extends Component {
                   margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis type="category" dataKey="city" name="city" />
-                  <YAxis type="number" dataKey="number" name="number of events" />
+                  <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
                   <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                   <Legend />
                   <Scatter data={this.getData()} fill="#8884d8" />
